f1e71d604d4d18b425bd726648acc375efee0523
Amin Bahrami <xauses@gmail.com>
2026-05-09 21:59:38 +0330

    electon

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    # Flask automatically converts dictionaries to JSON
+    return {"message": "Hello, World!"}
+
+if __name__ == "__main__":
+    app.run(debug=True)
