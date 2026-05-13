1eb3fb0f23dac9199b7016d5a985c9eb16ec00b3
Amin Bahrami <xauses@gmail.com>
2026-05-13 16:34:37 +0330

    aading db

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+
+# app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://user:password@localhost:5432/mydb"
+
+# # MySQL
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:up5FHoQOtbWjN88_F0r=@localhost:3306/bless"
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello_world():
